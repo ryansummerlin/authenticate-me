36cf1887501cdd745bcb3e8b3e7c060f8bee5fbb
ryansummerlin <ryansummerlin777@gmail.com>
2023-10-19 11:02:51 -0700

    minor changes to the api test routes

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -5,10 +5,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
